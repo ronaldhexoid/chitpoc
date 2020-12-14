13db9ce4bd43c7ccb7ba795c82aa5ff92a5bc367
unknown <ronald.hexooid@gmail.com>
2020-12-14 10:09:21 +0530

    first commit

--- Chits/Chits/settings.py
+++ Chits/Chits/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','ec2-54-157-4-216.compute-1.amazonaws.com']
 
 AUTH_USER_MODEL = 'authentication.User'
 
